a4ece58ddae65a73076f204657e4ac21bf5be7b0
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2026-04-22 13:21:13 +0100

    commit

--- ipconnex_telephony/ipconnex_telephony/ipconnex_telephony/doctype/daily_gain_summary/daily_gain_summary.py
+++ ipconnex_telephony/ipconnex_telephony/ipconnex_telephony/doctype/daily_gain_summary/daily_gain_summary.py
@@ -3,4 +3,16 @@ from frappe.model.document import Document
 
 
 class DailyGainSummary(Document):
-    pass
+    def before_save(self):
+        if not self.partner or not self.partner_type:
+            return
+        filter_field = "customer" if self.partner_type == "Customer" else "supplier"
+        contract = frappe.db.get_value(
+            "Telephony Partner Contract",
+            {filter_field: self.partner},
+            ["company", "sales_manager"],
+            as_dict=True,
+        )
+        if contract:
+            self.company = contract.company
+            self.sales_manager = contract.sales_manager
